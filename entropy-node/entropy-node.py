3b9c895dc096d2ebf41ba549a50445f9de24a425
Suraj Mishra <mishrasurajss123@gmail.com>
2026-09-14 23:34:06 +0530

    Solve entropy-node

---
+++ entropy-node/entropy-node.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def entropy_node(y: list[int]) -> float:
+    """
+    Returns the Shannon entropy as a Python float.
+    """
+    if len(y) == 0:
+        return 0.0
+    _, counts = np.unique(y, return_counts = True)
+    probs = counts/len(y)
+    entropy = -np.sum(probs * np.log2(probs))
+    return float(entropy)
+    pass
